2112b823dcc160ff3f80ef88da68eb6732d3e51b
lukelew <ailuqun313@gmail.com>
2018-09-09 22:21:27 +1000

    section1

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'plogging' });
 });
 
 module.exports = router;
