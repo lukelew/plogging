c3a2141d5e2524366de3189c9dff1417ec766ff1
lukelew <ailuqun313@gmail.com>
2018-09-25 21:35:22 +1000

    register page ui tiny modified

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/plogging',{ useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function(){
-  console.log('connected!')
+  console.log('Mongodb connected!')
 })
 
 module.exports = mongoose;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
 var User = require('../models/User');
 var router = express.Router();
 
@@ -8,23 +9,41 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/login',function(req, res){
+  res.render('login');
+})
 
 router.get('/register', function(req, res){
-  res.render('register')
+  res.render('register');
 })
 
 router.post('/register', function(req, res){
+  var name = req.body.name;
+  var gender = req.body.gender;
+  var email = req.body.email;
+  var password = req.body.password;
+
   var user = new User({
-    name: req.body.name,
-    gender: req.body.gender,
-    email: req.body.email,
-    password: req.body.password
+    name: name,
+    gender: gender,
+    email: email,
+    password: password
   });
 
-  user.save(function(err, callback){
-    if(err) return console.error(err);
-    console.log(callback);
-  })
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err){
+        console.log(err);
+      }
+      user.password = hash;
+      user.save(function(err, callback){
+        if(err) return console.error(err);
+        console.log(callback);
+      })
+      res.redirect('/users/login');
+    });
+  });
+
 })
 
 module.exports = router;
